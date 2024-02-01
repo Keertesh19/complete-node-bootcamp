37e86f90a600587f99f92a72e44edb2cf6268e49
Keertesh19 <keertesh.k98@gmail.com>
2024-02-01 12:51:15 -0500

    nodeJS: Section 2.10 : Sync and Async fn

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -7,3 +7,19 @@ const textOut = `This is what we know about the avacado ${textIn}.\nCreated on $
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log(`File written with the text \n"${textOut}"`);
 console.log(`Verification : ${fs.readFileSync("./txt/output.txt", "utf-8")}`);
+
+// Asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  if (err) return console.log("OOF! ");
+  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, text) => {
+    console.log(text);
+    fs.readFile("./txt/append.txt", "utf-8", (err, value) => {
+      console.log(value);
+
+      fs.writeFile("./txt/final.txt", `${text}\n${value}`, "utf-8", (err) => {
+        console.log(`File written successfully.`);
+      });
+    });
+  });
+});
+console.log("File File File");
